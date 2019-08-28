817cb9bef976c38c801209e45f4a85ab417d03a9
EduardoColon <eduardo.colon.15@gmail.com>
2019-08-28 11:48:12 -0600

    Lab 5
    script

---
+++ Lab5__28Agosto/js/script.js
@@ -0,0 +1,103 @@
+
+//javascript
+
+//1
+document.write("prueba 1");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//2
+var variable = "Prueba numero 2"
+document.write(variable);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//3
+var a = 2;
+var b = 2;
+var c = a + b;
+document.write(c);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//4
+
+//Este es un comentario en javascript
+document.write("Prueba 4");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//5
+
+var a = 10;
+document.write(a);
+document.write('\n');
+a++;
+a++;
+a++;
+document.write(a);
+document.write('\n');
+a--;
+a--;
+a--;
+a--;
+a--;
+a--;
+document.write(a);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//6
+
+var objeto = {nombre:"Eduardo", apellido:"Colon"};   
+
+document.write(objeto.nombre);
+document.write('\n');
+document.write(objeto.apellido);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//7
+var fecha = new Date();
+
+document.write(fecha);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//8
+
+document.write(Math.PI);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//9
+
+window.alert("Prueba numero 9");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//10
+
+var ingreso=prompt("Ingrese un valor","Texto default");
+
+document.write(ingreso);
+
+
